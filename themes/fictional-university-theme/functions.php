d08401903f6ba75ca7978ed0f9454b9a11b1e3a0
Prince Dickson <dicksoniprince@gmail.com>
2020-12-26 00:59:06 +0100

    remove api key

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -105,7 +105,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-   
+    $api['key'] = '';
     return $api;
 }
 
